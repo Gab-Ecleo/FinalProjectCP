2d4c7e914458f43e1e706578d91a4424a51d3c63
inaudibleoval@outlook.com <inaudibleoval@outlook.com>
2021-04-24 21:46:11 +0800

    Added Labels, Prices,Options ,Check Boxes ,Text boxes ,and one button on Form1 Design.

--- FinalProjectCP/Form1.cs
+++ FinalProjectCP/Form1.cs
@@ -16,5 +16,45 @@ namespace FinalProjectCP
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label23_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox11_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
